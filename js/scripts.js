92f1787c4d22410e99149bce1b18c7ae6e2dd268
Edgar Kibet <kibetedgar@gmail.com>
2018-05-25 13:34:08 +0300

    adds js pop up event and scripts to show entered user email and username

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+function userDetails(firstName,email){
+  this.name=firstName;
+  this.email=email;
+}
+userDetails.prototype.showName=function(){
+  return this.name;
+}
+userDetails.prototype.showEmail=function(){
+  return this.email;
+}
+//User interface
 $(document).ready(function() {
-
+setTimeout(popup, 500);
+function popup() {
+$("#logindiv").css("display", "block");
+}
 $('.thumbnail').hover(
     function(){
         $(this).find('.hoverOn').slideDown(250); //.fadeIn(250)
@@ -19,5 +33,17 @@ $(".list-inline-item").hover(
   );
   $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+});
+// Login form popup login-button click event.
+$("#loginbtn").click(function() {
+var name = $("#username").val();
+var email = $("#userEmail").val();
+var newUserDetails=new userDetails(name,email);
+
+$("#logindiv").css("display", "none");
+$("#toggleUname").append('<i class="fa fa-bell-o">'+'&nbsp;&nbsp;&nbsp;'+newUserDetails.name+"&nbsp;&nbsp;&nbsp;"+'<i class="fa fa-sort-down showEmail">'+'</i>');
+$("#toggleUname").click(function(){
+  $("#userMail").text(newUserDetails.email);
+});
 });
 });
